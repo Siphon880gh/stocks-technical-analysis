3f69310d08551f5f7a997767433cdea5f37b1e53
Siphon880gh <weng.f.fung@gmail.com>
2023-09-27 22:20:32 -0700

    Visual tweaks

--- popup.js
+++ popup.js
@@ -90,10 +90,28 @@ function promptAI() {
                 heading.style.textAlign = `center`;
                 heading.style.marginTop = `55px`;
 
+                var author = document.createElement(`p`);
+                author.innerHTML = `<div style="">
+                By Weng:<br/>
+                <div style="margin-top:5px;">
+                    <a target="_blank" href="https://www.github.com/Siphon880gh/" rel="nofollow">
+                        <img src="https://camo.githubusercontent.com/fbc3df79ffe1a99e482b154b29262ecbb10d6ee4ed22faa82683aa653d72c4e1/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f4769744875622d3130303030303f7374796c653d666f722d7468652d6261646765266c6f676f3d676974687562266c6f676f436f6c6f723d7768697465"
+                            data-canonical-src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&amp;logo=github&amp;logoColor=white" style="max-width: 100%; height:20px;">
+                    </a>
+                    <a target="_blank" href="https://www.linkedin.com/in/weng-fung/" rel="nofollow">
+                        <img src="https://camo.githubusercontent.com/0f56393c2fe76a2cd803ead7e5508f916eb5f1e62358226112e98f7e933301d7/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f4c696e6b6564496e2d626c75653f7374796c653d666c6174266c6f676f3d6c696e6b6564696e266c6162656c436f6c6f723d626c7565" alt="Linked-In" data-canonical-src="https://img.shields.io/badge/LinkedIn-blue?style=flat&amp;logo=linkedin&amp;labelColor=blue" style="max-width:100%;">
+                    </a>
+                    <a target="_blank" href="https://www.youtube.com/user/Siphon880yt/" rel="nofollow">
+                        <img src="https://camo.githubusercontent.com/0bf5ba8ac9f286f95b2a2e86aee46371e0ac03d38b64ee2b78b9b1490df38458/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f596f75747562652d7265643f7374796c653d666c6174266c6f676f3d796f7574756265266c6162656c436f6c6f723d726564" alt="Youtube" data-canonical-src="https://img.shields.io/badge/Youtube-red?style=flat&amp;logo=youtube&amp;labelColor=red" style="max-width:100%;">
+                    </a>
+                </div>
+            </div>`;
+
                 var content = document.createElement("div")
                 content.innerHTML = response;
 
                 container.append(heading);
+                container.append(author);
                 container.append(content);
                 return container;
 
